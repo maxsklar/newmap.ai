81b7a9a41045182b2619844d6f982a268d34f56f
Yuanxu Wu <sebastianwu@Yuanxus-MacBook-Pro.local>
2019-02-25 21:07:58 -0500

    add sbt server layer on top of original Interpreter

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+import ai.newmap.model._
+import ai.newmap.interpreter._
+import ai.newmap.interpreter.TypeChecker._
+import ai.newmap.util.{Outcome, Success, Failure}
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+	
+  def greet(code : String) = Action {
+    var envInterp = new EnvironmentInterpreter()
+    var response = envInterp(code)
+      response match {
+        case Success(s) => {
+          
+          println(s)
+          
+        }
+        case Failure(s) => println("Error:\n" + s)
+      }
+    Ok(""+response)
+  }
+
+}
