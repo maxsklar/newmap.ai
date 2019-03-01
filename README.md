533a427fac51fd479759e3f1cd3fabd2e3f1090e
Yuanxu Wu <sebastianwu@10-16-241-252.dynapool.wireless.nyu.edu>
2019-03-01 17:03:12 -0500

    add scripting feature

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ $ Heroku run web -a newmap-ai
 # Website Usage
 * Home page link: https://newmap-ai.herokuapp.com
 * Interpreter example link: https://newmap-ai.herokuapp.com/newmap?code=val%20a%20:%20Count%20=%205
+* Scripting interpreter example link: https://newmap-ai.herokuapp.com/newmap_script?code=val%20a:Count%20=%205;%20val%20b:a%20=%206
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -40,7 +40,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
         }
         case Failure(s) => println("Error:\n" + s)
       }
-    Ok("input: "+msg+" \n"+response)
+    Ok("Input: "+msg+" \n"+response)
   }
 
   def newmap_get(code : String) = Action {
@@ -55,6 +55,28 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
         case Failure(s) => println("Error:\n" + s)
       }
     Ok(""+response)
+  }
+
+  def newmap_script = Action { request: Request[AnyContent] =>
+    val body: AnyContent = request.body
+    val script = body.asFormUrlEncoded.get.get("text").get.head
+    var envInterp = new EnvironmentInterpreter()
+    var c : String = ""
+    var res :String = ""
+    for( c <-script.split(";") ){
+      res += "Input: "+c.replaceAll("^\\s+", "")+"; Output: "+envInterp(c)+" \n"
+    }
+    Ok(""+res)
+  }
+
+  def newmap_script_get(code : String) = Action {
+    var envInterp = new EnvironmentInterpreter()
+    var c : String = ""
+    var res :String = ""
+    for( c <-code.split(";") ){
+      res += "Input: "+c.replaceAll("^\\s+", "")+"; Output: "+envInterp(c)+" \n"
+    }
+    Ok(""+res)
   }
 
 }
