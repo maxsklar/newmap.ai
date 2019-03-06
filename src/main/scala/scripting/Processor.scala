3ebf3de153914b7ce347021f790e8bf940871298
tiannaxizhou <36940384+tiannaxizhou@users.noreply.github.com>
2019-03-05 19:34:29 -0500

    error handling

--- src/main/scala/scripting/Processor.scala
+++ src/main/scala/scripting/Processor.scala
@@ -2,34 +2,49 @@ package ai.newmap.scripting
 
 import scala.io.Source
 import util.control.Breaks._
+import java.io.{FileNotFoundException, IOException}
 
 import ai.newmap.interpreter.EnvironmentInterpreter
 import ai.newmap.util.{Outcome, Success, Failure}
 
 class Processor {
   def FileProcessor(filepath : String) = {
-    val file = Source.fromFile(filepath)
-    var output = ""
-    var linenum = 1
-    var breakflag = false
-    val envInterp = new EnvironmentInterpreter
-    breakable {
-      for (line <- file.getLines()) {
-        val result = envInterp(line)
-        result match {
-          case Failure(s) => {
-            breakflag = true
-            println(s"Error in line $linenum: $s")
-            }
-          case Success(s) => {
+    try {
+      val file = Source.fromFile(filepath)
+      var output = ""
+      var linenum = 1
+      var breakflag = false
+      val envInterp = new EnvironmentInterpreter
+      breakable {
+        val it = file.getLines
+        while (it.hasNext) {
+          var line = it.next()
+          while (line.length == 0 && it.hasNext) {
             linenum += 1
-            output += result + "\n"
+            line = it.next()
+          }
+          // further improvement for comments
+          // while(line.startsWith("#/*"))
+          val result = envInterp(line)
+          result match {
+            case Failure(s) => {
+              breakflag = true
+              println(s"Error in line $linenum: $s")
+            }
+            case Success(s) => {
+              linenum += 1
+              output += result + "\n"
             }
           }
-        if (breakflag) break
+          if (breakflag) break
+        }
+        println(output)
       }
-      println(output)
+      file.close()
     }
-    file.close()
+    catch {
+      case e: FileNotFoundException => println("File not found!")
+      case e: IOException => e.printStackTrace
+    }
   }
 }
