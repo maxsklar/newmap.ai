12a85dcdf1f884a2b854299d8d75abf85096c3b1
Yuanxu Wu <sebastianwu@Yuanxus-MBP.fios-router.home>
2019-03-20 15:09:45 -0400

    add wrapper for reset and hard reset functino

--- app/src/main/scala/interpreter/EnvironmentInterpreter.scala
+++ app/src/main/scala/interpreter/EnvironmentInterpreter.scala
@@ -14,6 +14,8 @@ import ai.newmap.environment.envCommenter.envComment
 import ai.newmap.environment.envCommiter.envCommit
 import ai.newmap.environment.envCommiter.printLog
 import ai.newmap.environment.envCommiter.checkout
+import ai.newmap.environment.envCommiter.resetHard
+import ai.newmap.environment.envCommiter.reset
 
 class EnvironmentInterpreter() {
   var env: Environment = Environment.Base
@@ -150,6 +152,28 @@ class EnvironmentInterpreter() {
     CommandPrintSomething(ret)
   }
 
+  def resetHardCommit(input: String): CommandInterpResponse = {
+    val cont = input.split("\\s+")
+    if (cont.size != 2){
+      return CommandPrintSomething("*Wrong input format, please check :help for detail*")
+    }
+    val uuid = cont(0)
+    val envAccessCode = cont(1)
+    val ret = resetHard(this.chanName, this.userName, uuid, envAccessCode)
+    CommandPrintSomething(ret)
+  }
+
+  def resetCommit(input: String): CommandInterpResponse = {
+    val cont = input.split("\\s+")
+    if (cont.size != 2){
+      return CommandPrintSomething("*Wrong input format, please check :help for detail*")
+    }
+    val uuid = cont(0)
+    val envAccessCode = cont(1)
+    val ret = reset(this.chanName, this.userName, uuid, envAccessCode)
+    CommandPrintSomething(ret)
+  }
+
 
   def applyInterpCommand(code: String): CommandInterpResponse = {
     code match {
@@ -175,6 +199,12 @@ class EnvironmentInterpreter() {
       case code if code.startsWith(":checkout ")=> {
                         this.checkoutVer(code.stripPrefix(":checkout "))
                       }
+      case code if code.startsWith(":hard reset ")=> {
+                        this.resetHardCommit(code.stripPrefix(":hard reset "))
+                      }
+      case code if code.startsWith(":reset ")=> {
+                        this.resetCommit(code.stripPrefix(":reset "))
+                      }
       //case ":env" => CommandPrintSomething(env.toString)
       case ":log off" => logOffEnv
       case ":printEnv" => CommandPrintSomething(envPrint(this.chanName, this.userName))
@@ -191,9 +221,11 @@ class EnvironmentInterpreter() {
         "*:printEnv*\tPrint the current environment\n" ++
         "*:copy <From chan name> <env name> <env password> <new env name> <new env password>*\tCopy an exist environment to a new environment\n"++
         "*:comment on <env name> <env password> (<comment>)*\tComment on an exist environment\n"++
-        "*:commit <comment>* or *:commit*\tcommit current environment\n"++
-        "*:printLog*\tprint the committed versions of current environment\n"++
+        "*:commit <comment>* or *:commit*\tCommit current environment\n"++
+        "*:printLog*\tPrint the committed versions of current environment\n"++
         "*:checkout <uuid>*\tCheck out privious committed version environment\n"++
+        "*:reset <commit id> <env password>*\tReset to the commit version with that commit id\n"++
+        "*:hard reset <commit id> <env password>*\t Reset to commit version with that commit id and delete afterwards versions\n"++
         "*:help*\tPrint this help message\n"
       )
       case _ => CommandPassThrough
