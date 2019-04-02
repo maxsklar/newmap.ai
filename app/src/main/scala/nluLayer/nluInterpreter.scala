200c324461cd5b8489ed196aa63ff406c55424f3
Yuanxu Wu <sebastianwu@Yuanxus-MacBook-Pro.local>
2019-04-02 15:47:38 -0400

    implement nlu interpret func to interpret action and action object

--- app/src/main/scala/nluLayer/nluInterpreter.scala
+++ app/src/main/scala/nluLayer/nluInterpreter.scala
@@ -26,6 +26,7 @@ object nluInterpreter {
 	val AWS_SECRET_KEY = envConstant.AWS_SECRET_KEY
 
 	val S3_ModelFileName_Prefix = "Model/"
+	val S3_CacheFileName_Prefix = "CACHE/"
 
 	val ActionMap:HashMap[String, String] = HashMap.empty[String,String]
 	val CreateIndMap:HashMap[String, String] = HashMap.empty[String, String]
@@ -92,5 +93,87 @@ object nluInterpreter {
   		println("*** create ds map ***")
   		CreateDsIndMap.forEach{case (key, value) => println (key + "-->" + value)}
  
+	}
+
+	def nluInterp(chanName:String, userName: String, code: String): String = {
+		val awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+  		val amazonS3Client = new AmazonS3Client(awsCredentials)
+
+		// read from nlu cache
+		var msg = ""
+		val nluCacheFileName = chanName+"_"+userName+"_nlu_cache.txt"
+		if(amazonS3Client.doesObjectExist(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)){
+			val nluCacheObj = amazonS3Client.getObject(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)
+			val nluCacheReader = new BufferedReader(new InputStreamReader(nluCacheObj.getObjectContent()))
+			val nluCacheLine = nluCacheReader.readLine
+			msg += nluCacheLine+" "
+		}
+		msg += code
+		// append msg
+
+		// interpete
+		println("*** "+code+" ***")
+		val cont = msg.toLowerCase().split("\\s+")
+		var actionType: String = ""
+		var gotActionType: Boolean = false
+		// check action kind
+		for(tok <- cont if !gotActionType){
+			//println(tok)
+			if(ActionMap.contains(tok)){
+				actionType = ActionMap(tok)
+				println("*** action type: "+actionType+" ***")
+				gotActionType = true
+			}
+		}
+
+		if(!gotActionType){
+			//println("*** no action type in message ***")
+			return "*Didn't recognize action in this message, please tell me exactly what you want to do*"
+		}else{
+			msg = actionType+" "
+		}
+
+		// check action object
+		var actObjectType: String = ""
+		var gotActObjectType: Boolean = false
+		actionType match {
+			case "create" => {
+				for(tok <- cont if !gotActObjectType){
+					if(CreateIndMap.contains(tok)){
+						actObjectType = CreateIndMap(tok)
+						println("*** action object type: "+actObjectType+" ***")
+						gotActObjectType = true
+					}
+				}
+				if(!gotActObjectType){
+					writeToCache(msg, nluCacheFileName)
+					//println("*** no action object type in message, please tell me what do u want to create env or data structure ***")
+					return "*Didn't recognize action object in message, please tell me what do u want to create, env or data structure*"
+				}else{
+					msg += actObjectType+" "
+					println("*** interpreted msg: "+msg+" ***")
+				}
+			}
+			case _ => {
+				println("*** fail because logic error, "+actionType+" ***")
+			}
+		}
+		println("*** finish interpret one message ***")
+		amazonS3Client.deleteObject(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)
+		return "*I understand what you want to do, you want to "+actionType+" a/an "+actObjectType+"*"
+		// check argument
+	}
+
+	// private
+	def writeToCache(str: String, cacheFileName: String) = {
+		val awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+  		val amazonS3Client = new AmazonS3Client(awsCredentials)
+
+		val cacheFile = new File(cacheFileName)
+		val cacheFileWriter = new FileWriter(cacheFile, false)
+		val cacheBufferedWriter = new BufferedWriter(cacheFileWriter)
+		cacheBufferedWriter.write(str)
+		cacheBufferedWriter.close()
+		amazonS3Client.putObject(BUCKET_NAME, S3_CacheFileName_Prefix+cacheFileName, cacheFile)
 	}
 }
