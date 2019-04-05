019d8a9cf1f1d8a6221a1fc4f4375b4b9e3d39d0
Nehal Agarwal <na2196@nyu.edu>
2019-04-05 12:51:41 -0400

    Controller Update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # newmap.ai
 This is the repo for the newmap.ai project: language and interpreter
+
+# Heroku Server Usage
+
+$ Heroku run web -a newmapai
+
+# Website Usage
+
+* Home page link: https://newmapai.herokuapp.com
+* Interpreter example link: https://newmapai.herokuapp.com/newmap?code=val%20a%20:%20Count%20=%205
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,6 +8,13 @@ import ai.newmap.model._
 import ai.newmap.interpreter._
 import ai.newmap.interpreter.TypeChecker._
 import ai.newmap.util.{Outcome, Success, Failure}
+import ai.newmap.environment.envReader.envRead
+import ai.newmap.environment.envReader.envLogIn
+import ai.newmap.environment.envCreater.envCreate
+import ai.newmap.environment.envPrinter.prettyPrinter
+
+import ai.newmap.nluLayer.trainer.train
+import ai.newmap.nluLayer.nluInterpreter.nluInterp
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -26,35 +33,139 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
-	
+
   def newmap = Action { request: Request[AnyContent] =>
-  	val body: AnyContent = request.body
-  	val msg = body.asFormUrlEncoded.get.get("text").get.head
-    var envInterp = new EnvironmentInterpreter()
-    var response = envInterp(msg)
-      response match {
-        case Success(s) => {
-          
-          println(s)
-          
-        }
-        case Failure(s) => println("Error:\n" + s)
+    val body: AnyContent = request.body
+    val msg = body.asFormUrlEncoded.get.get("text").get.head
+
+    // channel name:
+    val chanName = body.asFormUrlEncoded.get.get("channel_name").get.head
+    // user name:
+    val userName = body.asFormUrlEncoded.get.get("user_name").get.head
+    //Ok(chanName+" "+userName)
+    var response:String = ""
+    msg match{
+      case msg if (msg.startsWith(":create ") ||
+                    msg.startsWith(":log in ") ||
+                    msg.startsWith(":copy ") ||
+                    msg.startsWith(":comment on")||
+                    msg.startsWith(":commit") ||
+                    msg.startsWith(":checkout ") ||
+                    msg.startsWith(":reset ") ||
+                    msg.startsWith(":hard reset ") ||
+                    msg.startsWith(":script"))=>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
       }
-    Ok("input: "+msg+" \n"+response)
+      case ":printEnv" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
+      }
+      case ":envs" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
+      }
+      case ":log off" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
+      }
+      case ":printLog" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
+      }
+      case ":help" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(msg))
+      }
+      case _ =>{
+        response = ""+envRead(chanName, userName, msg)
+      }
+    }
+    Ok(">> "+msg+" \n"+response)
   }
 
   def newmap_get(code : String) = Action {
-    var envInterp = new EnvironmentInterpreter()
-    var response = envInterp(code)
-      response match {
-        case Success(s) => {
-          
-          println(s)
-          
-        }
-        case Failure(s) => println("Error:\n" + s)
+    // channel name:
+    val chanName = "testChanName"
+    // user name:
+    val userName = "admin"
+    //var envInterp = new EnvironmentInterpreter()
+    var response:String = ""
+    code match{
+      case code if (code.startsWith(":create ") ||
+                    code.startsWith(":log in ") ||
+                    code.startsWith(":copy ") ||
+                    code.startsWith(":comment on") ||
+                    code.startsWith(":commit") ||
+                    code.startsWith(":checkout ") ||
+                    code.startsWith(":reset ") ||
+                    code.startsWith(":hard set ") ||
+                    code.startsWith(":script"))=>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
       }
-    Ok(""+response)
+    case ":printEnv" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
+    }
+    case ":envs" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
+    }
+    case ":log off" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
+    }
+    case ":printLog" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
+    }
+    case ":help" =>{
+        var envInterp = new EnvironmentInterpreter()
+        envInterp.setChanName(chanName)
+        envInterp.setUserName(userName)
+        response = prettyPrinter(""+envInterp(code))
+    }
+    case _ =>{
+        response = ""+envRead(chanName, userName, code)
+      }
+    }
+    Ok(">> "+code+" \n"+response)
   }
 
+  def test_get(code : String) = Action {
+    // channel name:
+    val chanName = "testChanName"
+    // user name:
+    val userName = "admin"
+    if(code.equals("train")){
+      train
+      Ok("train finished")
+    }else{
+      val ret = nluInterp(chanName, userName, code)
+      Ok(ret)
+    }
+  }
 }
