86071bc6dc79c0735467dfc769403aef92eaf0da
Yuanxu Wu <sebastianwu@Yuanxus-MacBook-Pro.local>
2019-04-05 15:45:38 -0400

    change a way to parse argument

--- app/src/main/scala/nluLayer/argParser.scala
+++ app/src/main/scala/nluLayer/argParser.scala
@@ -16,7 +16,7 @@ import java.io.InputStreamReader
 import java.io.FileOutputStream
 import org.apache.commons.io.IOUtils
 import scala.collection.JavaConversions._
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.ListMap
 
 import ai.newmap.environment.envConstant
 
@@ -28,12 +28,14 @@ object argParser {
 	val S3_ModelFileName_Prefix = "Model/"
 	val S3_CacheFileName_Prefix = "CACHE/"
 
-	val CreateEnvIndMap:HashMap[String, String] = HashMap.empty[String,String]
-	val CreateDsIndMap:HashMap[String, String] = HashMap.empty[String,String]
+	val CreateEnvIndMap:ListMap[String, String] = ListMap.empty[String,String]
+	val CreateDsIndMap:ListMap[String, String] = ListMap.empty[String,String]
 
-	val LogInIndMap:HashMap[String, String] = HashMap.empty[String, String]
+	val LogInIndMap:ListMap[String, String] = ListMap.empty[String, String]
 
-	val CommentEnvIndMap:HashMap[String, String] = HashMap.empty[String, String]
+	val CheckOutIndMap:ListMap[String, String] = ListMap.empty[String, String]
+
+	val CommentEnvIndMap:ListMap[String, String] = ListMap.empty[String, String]
 
 	def parseCommentEnvArg(msg: String, nluCacheFileName: String): String = {
 		val awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
@@ -41,13 +43,14 @@ object argParser {
 
 		loadCommentEnvModel
 
-		val cont = msg.toLowerCase.split("\\s+")
+		val cont = msg.toLowerCase.split("\\s+").toList
 		var arg1 = ""	// env name
 		var gotArg1 = false
 		var arg2 = ""	// password
 		var gotArg2 = false
 		var arg3 = ""   // comment String
 		var gotArg3 = false
+/*
 		for(i <- 0 to cont.size-1 if (!gotArg1 || !gotArg2 || !gotArg3)){
 			val tok = cont(i)
 			if(CommentEnvIndMap.contains(tok)){
@@ -62,7 +65,7 @@ object argParser {
 				}
 				if(!gotArg3 && tmp.equals("3") && i < cont.size-1){
 					var index: Int = i+1
-					while(index < cont.size && !cont(index).equals("#")){
+					while(index < cont.size && cont(index) != "#"){
 						arg3 += cont(index)+" "
 						index += 1
 					}
@@ -70,15 +73,40 @@ object argParser {
 				}
 			}
 		}
+*/		
+		
+		//println(CommentEnvIndMap)
+		for((k,v) <- CommentEnvIndMap if(!gotArg1 || !gotArg2 || gotArg3)) {
+			if(cont.contains(k)){
+				val index = cont.indexOf(k)
+				val tmp = CommentEnvIndMap(k)
+				if(!gotArg1 && tmp.equals("1") && index < cont.size-1) {
+					arg1 = cont(index+1)
+					gotArg1 = true
+				}
+				if(!gotArg2 && tmp.equals("2") && index < cont.size-1) {
+					arg2 = cont(index+1)
+					gotArg2 = true
+				}
+				if(!gotArg2 && tmp.equals("3") && index < cont.size-1) {
+					var i: Int = index + 1
+					while(i < cont.size && cont(i) != "#"){
+						arg3 += cont(i)+" "
+						i += 1
+					}
+					gotArg3 = true
+				}
+			} 
+		}
 
 		if(!gotArg1){
-			nluInterpreter.writeToCache("comment on ", nluCacheFileName)
+			nluInterpreter.writeToCache("comment on env ", nluCacheFileName)
 			return "*I understand u want to comment, but missing env name, Please tell me a env name*"
 		}else if(!gotArg3){
-			nluInterpreter.writeToCache("comment on "+arg1+" with ", nluCacheFileName)
+			nluInterpreter.writeToCache("comment on  env "+arg1+" with ", nluCacheFileName)
 			return "I understand u want to comment, but missing comment string, Please tell me what u want to comment*"
 		}else if(!gotArg2){
-			nluInterpreter.writeToCache("comment on "+arg1+" with "+arg3+" # password ", nluCacheFileName)
+			nluInterpreter.writeToCache("comment on env "+arg1+" with "+arg3+" # password ", nluCacheFileName)
 			return "*Please tell me the password for this env*"
 		}else{
 			amazonS3Client.deleteObject(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)
@@ -93,12 +121,12 @@ object argParser {
 
 		loadLogInIndModel
 
-		val cont = msg.toLowerCase.split("\\s+")
+		val cont = msg.toLowerCase.split("\\s+").toList
 		var arg1 = ""	// env name
 		var gotArg1 = false
 		var arg2 = "" 	// password
 		var gotArg2 = false
-
+/*
 		for(i <- 0 to cont.size-1 if (!gotArg1 || !gotArg2)) {
 			val tok = cont(i)
 			if(LogInIndMap.contains(tok) && i < cont.size-1){
@@ -112,6 +140,21 @@ object argParser {
 					gotArg2 = true
 				}
 			}
+		}
+*/
+		for((k,v) <- LogInIndMap if(!gotArg1 || !gotArg2)) {
+			if(cont.contains(k)){
+				val index = cont.indexOf(k)
+				val tmp = LogInIndMap(k)
+				if(!gotArg1 && tmp.equals("1") && index < cont.size-1) {
+					arg1 = cont(index+1)
+					gotArg1 = true
+				}
+				if(!gotArg2 && tmp.equals("2") && index < cont.size-1) {
+					arg2 = cont(index+1)
+					gotArg2 = true
+				}
+			} 
 		}
 
 		if(!gotArg1){
@@ -134,11 +177,12 @@ object argParser {
 
   		loadCreateEnvIndModel
 
-		val cont = msg.toLowerCase.split("\\s+")
+		val cont = msg.toLowerCase.split("\\s+").toList
 		var arg1 = ""	// env name
 		var gotArg1 = false
 		var arg2 = ""	// password
 		var gotArg2 = false
+/*
 		for(i <- 0 to cont.size-1 if (!gotArg1 || !gotArg2)){
 			val tok = cont(i)
 			if(CreateEnvIndMap.contains(tok)){
@@ -153,6 +197,23 @@ object argParser {
 				}
 			}
 		}
+*/
+
+		for((k,v) <- CreateEnvIndMap if(!gotArg1 || !gotArg2)) {
+			if(cont.contains(k)){
+				val index = cont.indexOf(k)
+				val tmp = CreateEnvIndMap(k)
+				if(!gotArg1 && tmp.equals("1") && index < cont.size-1) {
+					arg1 = cont(index+1)
+					gotArg1 = true
+				}
+				if(!gotArg2 && tmp.equals("2") && index < cont.size-1) {
+					arg2 = cont(index+1)
+					gotArg2 = true
+				}
+			} 
+		}
+
 		var ret = ""
 		if(!gotArg1){
 			nluInterpreter.writeToCache(msg + " called ", nluCacheFileName)
@@ -171,6 +232,33 @@ object argParser {
 		println("*** Finish interpret a create env message! ***")
 		amazonS3Client.deleteObject(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)
 		ret+"\nInterpret finished. *"
+	}
+
+	def parseCheckOutArg(msg: String, nluCacheFileName: String): String = {
+		val awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+  		val amazonS3Client = new AmazonS3Client(awsCredentials)
+
+		loadCheckOutIndModel
+
+		val cont = msg.toLowerCase.split("\\s+").toList
+		var arg = ""
+		var gotArg = false
+
+		for((k,v) <- CheckOutIndMap if !gotArg){
+			if(cont.contains(k) && cont.indexOf(k) < cont.size-1){
+				arg = cont(cont.indexOf(k)+1)
+				gotArg = true
+			}
+		}
+
+		if(!gotArg){
+			nluInterpreter.writeToCache("show me the content with commit num ", nluCacheFileName)
+			return "*missing the commit number you want to check, Please tell me the commit number \n(you can type 'what's in the log' to check all commit numbers)*"
+		}else{
+			amazonS3Client.deleteObject(BUCKET_NAME, S3_CacheFileName_Prefix+nluCacheFileName)
+			return "*Got commit number "+arg+".* \nInterpret finished."
+		}
+
 	}
 
 	def loadCreateEnvIndModel() = {
@@ -244,6 +332,24 @@ object argParser {
 
   		println("*** commentEnv map ***")
   		CommentEnvIndMap.forEach{case (key, value) => println (key + "-->" + value)}
+ 	}
+
+ 	def loadCheckOutIndModel() = {
+ 		val awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+  		val amazonS3Client = new AmazonS3Client(awsCredentials)
+
+  		val checkOutIndFileName = "check_out_model.txt"
+  		val checkOutIndObj = amazonS3Client.getObject(BUCKET_NAME, S3_ModelFileName_Prefix+checkOutIndFileName)
+  		val checkOutIndReader = new BufferedReader(new InputStreamReader(checkOutIndObj.getObjectContent()))
+  		var checkOutIndLine = checkOutIndReader.readLine
+  		while(checkOutIndLine != null){
+  			val cont = checkOutIndLine.split(",")
+  			CheckOutIndMap += (cont(0) -> cont(1))
+  			checkOutIndLine = checkOutIndReader.readLine
+  		}
+
+  		println("*** check out map ***")
+  		CheckOutIndMap.forEach{case (key, value) => println (key + "-->" + value)}
  	}
 
 }
