70af1bce8e2db0d540e091e10d6c918b228c9d25
Yuanxu Wu <sebastianwu@10-16-176-11.dynapool.wireless.nyu.edu>
2019-04-10 18:48:57 -0400

    write some onboarding constant

---
+++ app/src/main/scala/nluLayer/onBoardConstant.scala
@@ -0,0 +1,24 @@
+package ai.newmap.nluLayer
+
+object onBoardConstant {
+	val ActRecommendation = "*Currently, these are the actions you can do in our system*\n"+
+							"*Create an environment*\n"+
+							"*Create a data structure*\n"+
+							"*Append on data structure*\n"+
+							"*Print the the envs in current channel*\n"+
+							"*Print the content in current env*\n"+
+							"*Print the log of current env*\n"+
+							"*Print the content of previous commit*\n"+
+							"*Log in*\n"+
+							"*Log off*\n"+
+							"*Comment on*\n"+
+							"*Commit*\n"+
+							"*Reset*\n"+
+							"*Hard Reset*\n"
+							"*Copy from*\n"
+
+	val GreetingConstant = "Hi, this is newmap.ai!\n"+
+						   "I'm a version system of data structure with a natural language operating interface\n"+
+						   "I'm here to help you improve the efficiency of your software developing\n"	
+}
+
