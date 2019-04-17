93b3e6e170b9c2a52155bf3bfc473d98b636a5c8
Yuanxu Wu <sebastianwu@10-16-143-120.dynapool.wireless.nyu.edu>
2019-04-17 17:49:01 -0400

    modify onboarding constant

--- app/src/main/scala/nluLayer/onBoardConstant.scala
+++ app/src/main/scala/nluLayer/onBoardConstant.scala
@@ -1,24 +1,25 @@
 package ai.newmap.nluLayer
 
 object onBoardConstant {
-	val ActRecommendation = "*Currently, these are the actions you can do in our system*\n"+
-							"*Create an environment*\n"+
-							"*Create a data structure*\n"+
-							"*Append on data structure*\n"+
-							"*Print the the envs in current channel*\n"+
-							"*Print the content in current env*\n"+
-							"*Print the log of current env*\n"+
-							"*Print the content of previous commit*\n"+
-							"*Log in*\n"+
-							"*Log off*\n"+
-							"*Comment on*\n"+
-							"*Commit*\n"+
-							"*Reset*\n"+
-							"*Hard Reset*\n"
-							"*Copy from*\n"
+	val ActRecommendation = """*Currently, these are the actions you can do in our system*\n"""+
+							"""*\tCreate an environment*\n"""+
+							"""*\tCreate a data structure*\n"""+
+							"""*\tAppend on data structure*\n"""+
+							"""*\tPrint the the envs in current channel*\n"""+
+							"""*\tPrint the content in current env*\n"""+
+							"""*\tPrint the log of current env*\n"""+
+							"""*\tPrint the content of previous commit*\n"""+
+							"""*\tLog in*\n"""+
+							"""*\tLog off*\n"""+
+							"""*\tComment on*\n"""+
+							"""*\tCommit*\n"""+
+							"""*\tReset*\n"""+
+							"""*\tHard Reset*\n"""
+							"""*\tCopy from*\n"""
 
-	val GreetingConstant = "Hi, this is newmap.ai!\n"+
-						   "I'm a version system of data structure with a natural language operating interface\n"+
-						   "I'm here to help you improve the efficiency of your software developing\n"	
+	val GreetingConstant = """Hi, this is newmap.ai!\n"""+
+						   """I'm a version system of data structure with a natural language operating interface\n"""+
+						   """I'm here to help you improve the efficiency of your software developing\n"""	+
+						   """Please type 'Help' for further using instructions :)"""
 }
 
