3311f8de1180a7edd29401741a7de0eef1320543
Yuanxu Wu <sebastianwu@Yuanxus-MacBook-Pro.local>
2019-04-17 13:37:12 -0400

    add nlu checker

---
+++ app/src/main/scala/nluLayer/nluChecker.scala
@@ -0,0 +1,45 @@
+package ai.newmap.nluLayer
+
+import ai.newmap.nluLayer.convTerminator.stopConv
+
+object nluChecker {
+
+	val CheckPrefix = """{
+    "response_type": "ephemeral",
+    """
+    val CheckSuffix = """
+	  "attachments": [
+	      {
+	          "text": "Did I understand it correct?",
+	          "callback_id": "interpret_checker",
+	          "color": "#3AA3E3",
+	          "attachment_type": "default",
+	          "actions": [
+	              {
+	                  "name": "Yes",
+	                  "text": "Yes",
+	                  "type": "button",
+	                  "value": "Yes"
+	              },
+	              {
+	                "name": "No",
+	                "text": "No",
+	                "type": "button",
+	                "value": "No"
+	              }
+	          ]
+	      }
+	  ]
+	}
+	"""
+
+	var Got = "_"
+	var GoingToGet = "_"
+
+	def generateJsonString(got: String, goingToGet: String): String = {
+		this.Got = got
+		this.GoingToGet = goingToGet
+		CheckPrefix+""" "text": """"+got+"""","""+CheckSuffix
+	}
+}
+
