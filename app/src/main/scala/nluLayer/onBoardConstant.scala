b09823786703bad26251c956e88c4a9a2bca5177
Yuanxu Wu <sebastianwu@10-16-143-120.dynapool.wireless.nyu.edu>
2019-04-17 19:45:05 -0400

    correct rec constant typo

--- app/src/main/scala/nluLayer/onBoardConstant.scala
+++ app/src/main/scala/nluLayer/onBoardConstant.scala
@@ -14,12 +14,12 @@ object onBoardConstant {
 							"""*\tComment on*\n"""+
 							"""*\tCommit*\n"""+
 							"""*\tReset*\n"""+
-							"""*\tHard Reset*\n"""
+							"""*\tHard Reset*\n"""+
 							"""*\tCopy from*\n"""
 
-	val GreetingConstant = """Hi, this is newmap.ai!\n"""+
+	val GreetingConstant = """Hi, this is *newmap* *.* *ai* !\n"""+
 						   """I'm a version system of data structure with a natural language operating interface\n"""+
-						   """I'm here to help you improve the efficiency of your software developing\n"""	+
+						   """I'm here to help you improve the efficiency of your software development\n"""	+
 						   """Please type 'Help' for further using instructions :)"""
 }
 
