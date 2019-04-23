7bd6dacbe14d9286e4502ecef286eafd45a24983
Yuanxu Wu <sebastianwu@Yuanxus-MacBook-Pro.local>
2019-04-23 12:56:53 -0400

    update onboarding constant

--- app/src/main/scala/nluLayer/onBoardConstant.scala
+++ app/src/main/scala/nluLayer/onBoardConstant.scala
@@ -17,7 +17,7 @@ object onBoardConstant {
 							"""*\tHard Reset*\n"""+
 							"""*\tCopy from*\n"""
 
-	val GreetingConstant = """Hi, this is *newmap* *.* *ai* !\n"""+
+	val GreetingConstant = """Hi, this is newmap.ai !\n"""+
 						   """I'm a version system of data structure with a natural language operating interface\n"""+
 						   """I'm here to help you improve the efficiency of your software development\n"""	+
 						   """Please type 'Help' for further using instructions :)"""
