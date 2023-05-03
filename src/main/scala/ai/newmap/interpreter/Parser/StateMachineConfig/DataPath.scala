f1fbb1695772ecf44f26ac045a21bf91150b7dcd
Maya Balaji <metallicmay@gmail.com>
2023-05-03 18:37:02 -0400

    updte data path

--- src/main/scala/ai/newmap/interpreter/Parser/StateMachineConfig/DataPath.scala
+++ src/main/scala/ai/newmap/interpreter/Parser/StateMachineConfig/DataPath.scala
@@ -3,7 +3,7 @@ package ai.newmap.interpreter.Parser.StateMachineConfig
 import ai.newmap.StateMachine.{State, Transition}
 import ai.newmap.interpreter.Lexer
 import ai.newmap.interpreter.Lexer.Identifier
-import ai.newmap.model.{EnvStatementParse, IterateIntoStatementParse, ParseElement, ParseTree}
+import ai.newmap.model._
 
 import scala.collection.mutable.ListBuffer
 
@@ -13,13 +13,13 @@ object DataPath {
   private val dataIdentifier = new State(isEndState = false, name = "dataIdentifier")
   private val dataIdentifierIdentifier = new State(isEndState = false, name = "dataIdentifierIdentifier")
   private val dataIdentifierIdentifierIdentifier = new State(isEndState = false, name = "dataIdentifierIdentifierIdentifier")
-  private val dataEndState = new IterateEndState(name = "dataEndState")
+  private val dataEndState = new DataEndState(name = "dataEndState")
 
   val dataInitTransition = new Transition(expectedToken = Identifier("data"), nextState = initState)
   private val dataId1Transition = new Transition(expectedTokenClass = classOf[Identifier], nextState = dataIdentifier)
   private val dataId2Transition = new Transition(expectedTokenClass = classOf[Identifier], nextState = dataIdentifierIdentifier)
   private val dataId3Transition = new Transition(nextExpectedParseTree = classOf[ParseTree], nextState = dataIdentifierIdentifierIdentifier)
-  private val dataEndTransition = new IterateEndStateTransition(nextState = dataEndState)
+  private val dataEndTransition = new DataEndStateTransition(nextState = dataEndState)
 
   initState.addAcceptedTransition(dataId1Transition)
   dataIdentifier.addAcceptedTransition(dataId2Transition)
@@ -28,24 +28,25 @@ object DataPath {
 
 }
 
-class IterateEndState(name:String) extends State(isEndState = true, name){
+class DataEndState(name: String) extends State(isEndState = true, name) {
 
   var tokenOptions: Option[List[ParseElement]] = None
-  override def reach(p: ListBuffer[ParseElement], ts:Seq[Lexer.Token] = null): Unit = {
+
+  override def reach(p: ListBuffer[ParseElement], ts: Seq[Lexer.Token] = null): Unit = {
     tokenOptions = Option(p.toList)
   }
 
   override def generateParseTree: EnvStatementParse = {
     val tokens = tokenOptions.get
-    IterateIntoStatementParse(
-      tokens(1).asInstanceOf[Identifier],
-      tokens(2).asInstanceOf[Identifier]
+    NewTypeStatementParse(
+      tokens(1).asInstanceOf[IdentifierParse],
+      tokens(2).asInstanceOf[ParseTree]
     )
   }
 
 
 }
 
-class IterateEndStateTransition(nextState:State) extends Transition(expectedToken = null, nextState = nextState){
+class DataEndStateTransition(nextState: State) extends Transition(expectedToken = null, nextState = nextState) {
   override def validateToken(t: Lexer.Token): Boolean = true
 }
