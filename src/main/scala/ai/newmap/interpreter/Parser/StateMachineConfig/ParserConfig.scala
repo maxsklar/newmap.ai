45dc30d7fe4d067444dd5541af4b159239464243
Ajeeta Asthana <ajita.asthana@gmail.com>
2023-05-03 14:06:18 -0400

    ApplyCommandPath changes for adding expactation and depth

--- src/main/scala/ai/newmap/interpreter/Parser/StateMachineConfig/ParserConfig.scala
+++ src/main/scala/ai/newmap/interpreter/Parser/StateMachineConfig/ParserConfig.scala
@@ -6,7 +6,7 @@ import ai.newmap.interpreter.Parser.StateMachineConfig.DisconnectChannelPath.dis
 import ai.newmap.interpreter.Parser.StateMachineConfig.IteratePath.iterateInitTransition
 import ai.newmap.interpreter.Parser.StateMachineConfig.ConnectChannelPath.connectChannelInitTransition
 import ai.newmap.interpreter.Parser.StateMachineConfig.ForkChannelPath.forkedVersionedStmtInitTransition
-//import ai.newmap.interpreter.Parser.StateMachineConfig.applyCommandPath.applyCommandStmtInitTransition
+import ai.newmap.interpreter.Parser.StateMachineConfig.applyCommandPath.applyCommandStmtInitTransition
 import ai.newmap.util.Outcome
 
 class ParserConfig() {
@@ -16,7 +16,7 @@ class ParserConfig() {
   initState.addAcceptedTransition(iterateInitTransition)
   initState.addAcceptedTransition(connectChannelInitTransition)
   initState.addAcceptedTransition(forkedVersionedStmtInitTransition)
-//  initState.addAcceptedTransition(applyCommandStmtInitTransition)
+  initState.addAcceptedTransition(applyCommandStmtInitTransition)
 }
 
 object StateMachineRunner{
